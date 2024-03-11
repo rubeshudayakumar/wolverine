774e28435d68cfa85f24aadaae8ec1b0a64bcfe7
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2024-03-11 15:33:21 +0530

    engine start stop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,46 @@
-$(document).ready(function () {
-  console.log("DOM Loaded");
+$(document).ready(() => {
+  $("body").keyup(function () {});
+
+  var lastKeyPressTime = 0;
+  var lastKeyCode = null;
+  var timeout = 300;
+
+  const keyActions = {
+    s: startEngine,
+    e: offEngineRemoveKey,
+  };
+
+  const doubleInputKeys = ["e"];
+
+  $(document).on("keydown", function (e) {
+    const carAudio = $("#car-audio");
+    var currentTime = new Date().getTime();
+    if (
+      e.which == lastKeyCode &&
+      currentTime - lastKeyPressTime < timeout &&
+      doubleInputKeys.includes(e.key)
+    ) {
+      keyActions[e.key](carAudio);
+    } else {
+      keyActions[e.key](carAudio);
+    }
+    lastKeyPressTime = currentTime;
+    lastKeyCode = e.which;
+  });
 });
+
+const startEngine = (carAudio) => {
+  carAudio.attr("src", "../assets/audio/car-start.mp3");
+  carAudio[0].play();
+  setTimeout(() => {
+    carAudio.attr("src", "../assets/audio/car-idle.mp3");
+    carAudio.attr("loop", true);
+    carAudio[0].play();
+  }, 2500);
+};
+
+const offEngineRemoveKey = (carAudio) => {
+  carAudio.attr("src", "../assets/audio/car-off.mp3");
+  carAudio.attr("loop", false);
+  carAudio[0].play();
+};
